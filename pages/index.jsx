d04a0166ad89b37dd8a4390162c794bb384fce35
AvetisDN <avetisdn@gmail.com>
2022-01-20 17:57:24 +0300

    1

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="w-full max-w-3xl bg-gray-800 p-4 md:p-6 rounded-none md:rounded-2xl shadow-lg shadow-[rgba(0,0,0,.3)]">
+      <h1 className="text-6xl">
+        Hello World
+      </h1>
+    </div>
+  )
+}
